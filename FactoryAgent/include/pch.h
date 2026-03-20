b9aef980f70c923cf2a63ffbc08483ddcf2809ae
Bhavik <Bhavik@BHAVIK-LAPTOP>
2026-03-20 20:09:10 +0530

    Pch added

---
+++ FactoryAgent/include/pch.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// Target Windows 10
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+
+// Windows headers
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <winhttp.h>
+#include <commctrl.h>
+
+// C++ STL
+#include <string>
+#include <vector>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <unordered_map>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <optional>
+#include <functional>
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+
+// Third-party
+#include <json/json.hpp>
---
+++ FactoryAgent/src/pch.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+// This file is used to build the precompiled header .pch file
